2a1f583b01dc997b14c89e7757ec14aa7f24ae65
Josh O Farrell <hurleyjosh94@gmail.com>
2015-03-12 10:35:22 +0000

    $("#enemy1").animate({left:400},1240+'px')
    animated background image

--- script.js
+++ script.js
@@ -18,14 +18,20 @@ $(document).ready(function(){
 		
 	if($(window).scrollTop() >=14200)
 		{
-			$("#police box").animate({left:300},11100+'px');
+			$("#police box").animate({left:300},111+'px');
 			$("#background").css('top',$(window).scrollTop()*14000+'px');
 			$("#thing").css('top',$(window).scrollTop()*14000+'px');
-		    $("#fact1").css('left',$(window).scrollTop()*11100+'px');
-			$("#enemy1").css('top',$(window).scrollTop()*12400+'px');
+		}	
+		
+		if($(window).scrollTop() >= 15100)
+		{
+		    $("#fact1").css('left',$(window).scrollTop()*1200+'px');
+			
 		}
+		
 	if($(window).scrollTop()>=1240)
 	{
+		$("#enemy1").animate({left:400},1240+'px');
 		$("#deadly").animate({left:300},1200+'px'); 
 		$("#deadly2").animate({left:350},1200+'px');
         $("#deadly3").animate({left:300},1200+'px');
@@ -33,7 +39,7 @@ $(document).ready(function(){
 	
 	if($(window).scrollTop() >=1420)
 	{
-		$("#cybermen box").css('left',top*13400+'px');
+		$("#cybermen box").animate({left:300},1400+'px');
 		$("#robots").animate({left:300},1400+'px');
 	}
 		if($(window).scrollTop() >=15400)
